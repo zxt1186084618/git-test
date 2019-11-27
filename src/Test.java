3f7b9c223d008b3edf89c99c7b004e6eedfb8f8b
zxt1186084618 <1186084618@qq.com>
2019-11-27 21:15:34 +0800

    修改提交

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("你好");
+        System.out.println("你好啊");
     }
 }
